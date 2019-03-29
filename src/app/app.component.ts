9f781222cbdbab2ec33c7b2ca0224ed973b5a588
Jéssyca <jessycarozeene@gmail.com>
2019-03-29 18:56:14 -0300

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { HttpClient } from '@angular/common/http';
+
+export interface Character {
+  name: string;
+  image: string;
+  fatality: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  myList: Character[]
+  confirmList: Character[] = [];
+
+  constructor(private httpClient: HttpClient) {
+    this.getMyList()
+  }
+
+  getMyList() {
+     this.httpClient.get<Character[]>("assets/data.json")
+     .subscribe(list =>{
+      this.myList = list;
+     })
+  }
+
+  drop(event: CdkDragDrop<Character[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+
+      transferArrayItem(event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex);
+    }
+  }
+
+}
